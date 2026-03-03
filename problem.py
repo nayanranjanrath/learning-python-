e20703d301dddc4b7bb0841d9da518f032d55179
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-03-03 10:55:16 +0530

    used format in print

--- problem.py
+++ problem.py
@@ -1,15 +1,26 @@
-age = int(input("enter your age "))
-if( age in range(14)):
-    print("you are a kid ")
+# age = int(input("enter your age "))
+# if( age in range(14)):
+#     print("you are a kid ")
 
-elif  (age in range(13,20)):
-    print("you are a teenager ")
+# elif  (age in range(13,20)):
+#     print("you are a teenager ")
 
-elif (age in range(20,60)):
-    print("you are an adult ")
+# elif (age in range(20,60)):
+#     print("you are an adult ")
 
-elif (age>=60):
-    print("you are senior ")
+# elif (age>=60):
+#     print("you are senior ")
 
+# else:
+#     print("age is in valid ")
+
+
+age =int(input("tell me your age "))
+day = "mon day"
+
+price = 12 if age>=18 else 8
+if (day=="wednes day"):
+    price=price-2
+    print("your ticket price is =",price )
 else:
-    print("age is in valid ")
+    print("price is {}".format(price))
