ad544d18848b89cc67c4b06bf51faed304bff94f
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-03-04 23:13:23 +0530

    learning about the scope

--- usingfuction.py
+++ usingfuction.py
@@ -1,6 +1,17 @@
-def circledata(r):
-    circumference =2*3.14*r
-    area = 3.14*r*r
-    return circumference,area
+# def circledata(r):
+#     circumference =2*3.14*r
+#     area = 3.14*r*r
+#     return circumference,area
 
-print(circledata(5))
+# print(circledata(5))
+
+
+
+def first (num):
+    print(num)
+    def second(x):
+        return x**num
+    return second
+
+f=first(2)
+print(f(4))
