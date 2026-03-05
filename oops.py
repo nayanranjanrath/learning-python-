53053e7bcc8aba725d730833a2dd894d55f259d0
nayanranjanrath <nayanranjanrath5@gmail.com>
2026-03-05 08:53:41 +0530

    use of inheritance

--- oops.py
+++ oops.py
@@ -8,10 +8,17 @@ class cars ():
         return self.brand+self.model
 
 
+class e_cars(cars):
+    def __init__(self, brand, model,batterysize):
+        super().__init__(brand, model)
+        self.batterysize = batterysize
+
 my_car =cars("toyota","corella")
 print(my_car.brand)
 print(my_car.model)
 print(my_car.full_name())
  
+my_ev =e_cars("tesla","model s","85 Kwh")
+print(my_ev.brand,my_ev.model,my_ev.full_name())
  
 
